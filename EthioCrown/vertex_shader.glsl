58a0924ccee3f1d16a906df184bf7c07bc343157
Fellasfaw <fellasfaw@outlook.com>
2024-12-28 20:28:25 +0300

    Add vertex normals and update shader transformations

--- EthioCrown/vertex_shader.glsl
+++ EthioCrown/vertex_shader.glsl
@@ -1,10 +1,18 @@
 #version 330 core
-layout (location = 0) in vec3 aPos;
+layout(location = 0) in vec3 aPos;   // Vertex position
+layout(location = 1) in vec3 aNormal; // Vertex normal
+
+out vec3 FragPos;  // Position for fragment shader
+out vec3 Normal;   // Normal for fragment shader
 
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-void main() {
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+void main()
+{
+    FragPos = vec3(model * vec4(aPos, 1.0));  // Transform vertex position
+    Normal = mat3(transpose(inverse(model))) * aNormal; // Correct normals
+
+    gl_Position = projection * view * vec4(FragPos, 1.0);
 }
